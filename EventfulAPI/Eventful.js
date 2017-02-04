2237231ad0366261dcf6a0ee49f4ce5b12fffe5e
Grant Smith <smithge87@gmail.com>
2017-02-04 09:42:32 -0600

    adding html and js for yelp and eventful interactions

---
+++ EventfulAPI/Eventful.js
@@ -0,0 +1,34 @@
+$('form').submit(function(event) {
+  event.preventDefault();
+
+     var oArgs = {
+        app_key: "DzmC74sw8ngVbbrr",
+        where : $('#cityInput').val(),
+        q : $('#keywordInput').val(),
+        page_size: 10,
+        sort_order: "popularity",
+     };
+     EVDB.API.call("/events/search", oArgs, function(oData) {
+        parseData(oData);
+      });
+
+});
+
+
+function parseData(data){
+  for (let i=0;i<data.events.event.length;i++){
+    currentResults = formatRow(data["events"]["event"][(i.toString())]);
+      $("#resultsTable").append(currentResults);
+  }
+}
+function formatRow(object){
+  cleanedObject = "<tr><td><form> <input type="+"'"+"radio"+"' "+"name="+"'"+"select"+"' "+"value="+"'"+"other"+"'"+
+  "></form></td><td><img src="+object["image"]["medium"]["url"]+
+  "><td><b>"+object.title+
+  "</b></td><td>"+object["start_time"]+
+  "</td><td>"+object["venue_name"]+
+  "</td><td><a href="+object["url"]+
+  ">"+"More info"+"</a></td>"+
+  "</tr>";
+  return cleanedObject;
+}
