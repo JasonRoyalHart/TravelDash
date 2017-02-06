066c10e25c1458bcc89cfe332e4de7b573225811
Grant Smith <smithge87@gmail.com>
2017-02-06 09:45:06 -0600

    added basic view navigation with booking and dashboard controller

---
+++ TravelDash/Controllers/BookingController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelDash.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult FlightsIndex()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult FlightsIndex(model)
+        //{
+        //    return View();
+        //}
+        public ActionResult CarsIndex()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult CarsIndex()
+        //{
+        //    return View();
+        //}
+        public ActionResult HotelsIndex()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult HotelsIndex()
+        //{
+        //    return View();
+        //}
+        public ActionResult RestaurantsIndex()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult RestaurantsIndex()
+        //{
+        //    return View();
+        //}
+        public ActionResult EventsIndex()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult EventsIndex()
+        //{
+        //    return View();
+        //}
+    }
+}
