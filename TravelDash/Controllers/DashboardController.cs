c8cac215d623cf931aeb043105d9f3e483db70f3
Grant Smith <smithge87@gmail.com>
2017-02-06 14:56:59 -0600

    added DashboardViewModel and passed username to be displayed in the view

--- TravelDash/Controllers/DashboardController.cs
+++ TravelDash/Controllers/DashboardController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelDash.Models;
 
 namespace TravelDash.Controllers
 {
     public class DashboardController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public DashboardController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var currentUserName = User.Identity.Name;
+            var currentUser = _context.Users.FirstOrDefault(m => m.UserName == currentUserName);
+            ViewBag.Dashboard = currentUser.firstName;
+
+        return View(ViewBag);
         }
     }
+    
 }
---
+++ TravelDash/Models/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelDash.Models
+{
+    public class DashboardViewModel
+    {
+        private ApplicationDbContext _context;
+
+        public DashboardViewModel()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
