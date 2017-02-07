9fee6c924cf00bd48748f917749c5000947eee79
JenRules <goldenruel@hotmail.com>
2017-02-07 14:24:13 -0600

    created plane, car, and hotel models

---
+++ TravelDash/Migrations/201702072019460_hotelmodel.cs
@@ -0,0 +1,16 @@
+namespace TravelDash.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class hotelmodel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
