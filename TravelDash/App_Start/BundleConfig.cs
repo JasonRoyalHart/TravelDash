e1681478e2af7ad28e9b2f94d2c6c4968370d4d8
JenRules <goldenruel@hotmail.com>
2017-02-09 08:18:12 -0600

    updated background

--- TravelDash/App_Start/BundleConfig.cs
+++ TravelDash/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TravelDash
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrapcer.css",
                       "~/Content/site.css"));
         }
     }
