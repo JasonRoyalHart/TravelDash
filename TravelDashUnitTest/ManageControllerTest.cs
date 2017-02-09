eebf7fb615ef7e0ddf8c2df179cd02263368e6c2
ton <bojingo10@gmail.com>
2017-02-09 08:29:45 -0600

    added more testing

---
+++ TravelDashUnitTest/ManageControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using TravelDash;
+using TravelDash.Controllers;
+
+namespace TravelDashUnitTest
+{
+    [TestClass]
+    public class ManageControllerTest
+    {
+        [TestMethod]
+        public void TestAddPhoneNumber()
+        {
+            //Arrange
+            ManageController controller = new ManageController();
+            //Act
+            ViewResult result = controller.AddPhoneNumber() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestChangePassword()
+        {
+            //Arrange
+            ManageController controller = new ManageController();
+            //Act
+            ViewResult result = controller.ChangePassword() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestSetPassword()
+        {
+            //Arrange
+            ManageController controller = new ManageController();
+            //Act
+            ViewResult result = controller.SetPassword() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+    }
+}
