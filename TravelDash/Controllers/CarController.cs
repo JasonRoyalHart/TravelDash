c50467fbaaf8cc5e2fb3fc2575fd0b1fa6e2dfdd
Grant Smith <smithge87@gmail.com>
2017-02-08 21:54:27 -0600

    added functionality to search for cars from form on cars controller

--- TravelDash/Controllers/CarController.cs
+++ TravelDash/Controllers/CarController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TravelDash.Models;
@@ -18,7 +21,17 @@ namespace TravelDash.Controllers
         // GET: Car
         public ActionResult Index()
         {
-            return View();
+            var currentUserName = User.Identity.Name;
+            var currentUser = _context.Users.FirstOrDefault(m => m.UserName == currentUserName);
+            var currentLocation = _context.TripModels.FirstOrDefault(m => m.UserId == currentUser.Email);
+            ViewBag.Location = currentLocation.Destination;
+            ViewBag.User = currentUser.Email;
+            var restList = _context.TempCars;
+            var viewModel = new CarsViewModel()
+            {
+                TempCars = restList
+            };
+            return View(viewModel);
         }
         //[HttpPost]
         //[ValidateAntiForgeryToken]
@@ -26,5 +39,46 @@ namespace TravelDash.Controllers
         //{
         //    return View();
         //}
+        public ActionResult CarSearchResult(CarsViewModel model)
+        {
+            var currentUserName = User.Identity.Name;
+            var currentUser = _context.Users.FirstOrDefault(m => m.UserName == currentUserName);
+            string pickup = model.Pickup;
+            string pickUpDate = model.PickUpDtae;
+            string dropOffDate = model.DropOffDate;
+            string api = "bG7HZQsZILLmjpOhLihxd9K4CMGGJsWG";
+            string URL = "https://api.sandbox.amadeus.com/v1.2/cars/search-airport?apikey=" + api;
+            URL += "&location=" + pickup;
+            URL += "&pick_up=" + pickUpDate;
+            URL += "&drop_off=" + dropOffDate;
+            WebRequest request = WebRequest.Create(URL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            JObject responseFromServer = JObject.Parse(reader.ReadToEnd());
+            _context.TempHotels.RemoveRange(_context.TempHotels);
+            _context.SaveChanges();
+
+            // This next bit needs to be changed to store the values we want to pull from results and display per object
+
+            //for (int i = 0; i < Int32.Parse(Results); i++)
+            //{
+            //    var hotel = new TempHotels()
+            //    {
+            //        UserId = currentUser.Email,
+            //        property_name = responseFromServer["results"][i]["property_name"].ToString(),
+            //        property_code = responseFromServer["results"][i]["property_code"].ToString(),
+            //        //              property_code = responseFromServer["results"][0]["property_code"].ToString(),
+            //        address = responseFromServer["results"][i]["address"]["line1"].ToString(),
+            //        total_price = responseFromServer["results"][i]["total_price"]["amount"].ToString()
+            //    };
+            //    _context.TempHotels.Add(hotel);
+            //    _context.SaveChanges();
+            //}
+
+            return RedirectToAction("CarsIndex", "Car");
+        }
     }
 }
---
+++ TravelDash/Models/CarsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelDash.Models
+{
+    public class CarsViewModel
+    {
+        private ApplicationDbContext _context;
+
+        public CarsViewModel()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        [Required]
+        [Display(Name = "Pickup")]
+        public string Pickup { get; set; }
+
+        [Required]
+        [Display(Name = "Pick up Date")]
+        public string PickUpDtae { get; set; }
+
+        [Required]
+        [Display(Name = "Drop Off Date")]
+        public string DropOffDate { get; set; }
+
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public CarModel Cars { get; set; }
+        public IEnumerable<TempCars> TempCars { get; set; }
+    }
+}
